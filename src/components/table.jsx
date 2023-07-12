49ec98cff758199b81da8549f42708e78c53718c
AntonAnatoliyovich <anton.shadura@gmail.com>
2023-07-12 21:55:19 +0300

    added sort

--- src/components/table.jsx
+++ src/components/table.jsx
@@ -1,15 +1,59 @@
-export const Table = ({boxers}) => {
+import React from "react";
+
+export const Table = (boxer) => {
+
+    const {boxers} = boxer;
+    const [sortedField, setSortedField] = React.useState(null);
+    let sortedBoxers = [...boxers];
+    sortedBoxers.sort((a,b) => {
+        if (a[sortedField] < b[sortedField]) {
+            return -1;
+        }
+        if (a[sortedField] > b[sortedField]) {
+            return 1;
+        }
+        return 0;
+    })
+
     return (
         <table className="table">
             <thead>
                 <tr>
-                    <th scope="col">id</th>
-                    <th scope="col">name</th>
-                    <th scope="col">age</th>
-                    <th scope="col">nationality</th>
-                    <th scope="col">stance</th>
-                    <th scope="col">division</th>
-                    <th scope="col">record</th>
+                    <th scope="col">
+                        <button type="button" onClick={() => setSortedField('id')}>
+                            id
+                        </button>
+                    </th>
+                    <th scope="col">
+                        <button type="button" onClick={() => setSortedField('name')}>
+                            name
+                        </button>
+                    </th>
+                    <th scope="col">
+                        <button type="button" onClick={() => setSortedField('age')}>
+                            age
+                        </button>
+                    </th>
+                    <th scope="col">
+                        <button type="button" onClick={() => setSortedField('nationality')}>
+                            nationality
+                        </button>
+                    </th>
+                    <th scope="col">
+                        <button type="button" onClick={() => setSortedField('stance')}>
+                            stance
+                        </button>
+                    </th>
+                    <th scope="col">
+                        <button type="button" onClick={() => setSortedField('division')}>
+                            division
+                        </button>
+                    </th>
+                    <th scope="col">
+                        <button type="button" onClick={() => setSortedField('record')}>
+                            record
+                        </button>
+                    </th>
                 </tr>
             </thead>
             <tbody>
